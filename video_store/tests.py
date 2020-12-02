9386657d43705ab97d9b72248c74e3ae515e1351
sushil sarwade <>
2020-12-02 20:18:12 +0530

    adding circleci

--- video_store/tests.py
+++ video_store/tests.py
@@ -1,6 +1,16 @@
 from django.test import TestCase
-
+from video_store.models import Video, APIKey
 # Create your tests here.
 
-# def VideoAPITest(APITestCase):
-#     pass
+def VideoAPITestCase(TestCase):
+    
+    def setUp(self):
+        self.api_key = APIKey.objects.create(api_key='abcdefghi')
+    
+    def test_createvideo(self):
+        Video.objects.create(youtube_id='1234', title='asda', description='asdasd')
+        assert True
+    
+    def test_createapikey(self):
+        APIKey.objects.create(api_key='asdaosdimo')
+        assert True
